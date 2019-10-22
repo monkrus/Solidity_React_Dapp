bf4f34961afd19750610c858322281daa664e00c
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-10-22 20:34:35 +0200

    - done

--- src/App.js
+++ src/App.js
@@ -6,19 +6,10 @@ class App extends Component {
     render() {
         return (
             <div className="container">
-                <h1> bglkeelwjf </h1>
+                <h1>Hello, world</h1>
             </div>
         );
     }
-}
-
-
-function App() {
-    return (
-        <div className="container">
-            <h1> bglkeelwjf </h1>
-        </div>
-    );
 }
 
 export default App;
