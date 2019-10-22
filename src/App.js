eae281e44e78310661c59154ca301b719adb7137
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-10-22 16:53:30 +0200

    - in progress

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Web3 from 'web3';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1> bglkeelwjf </h1>
+            </div>
+        );
+    }
+}
+
+
+function App() {
+    return (
+        <div className="container">
+            <h1> bglkeelwjf </h1>
+        </div>
+    );
+}
+
+export default App;
