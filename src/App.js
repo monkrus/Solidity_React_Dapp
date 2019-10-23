2f11add9daeff9eac479e11e48102c400d83bd8a
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-10-23 12:13:42 +0200

    :grin:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import Web3 from 'web3';
 import './App.css';
 
 class App extends Component {
+
+    componentWillMount() {
+        this.loadBlockchainData()
+    }
+
+    async loadBlockchainData() {
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+        const network = await web3.eth.net.getNetworkType();
+        console.log("network:", network)
+    }
+
     render() {
         return (
             <div className="container">
